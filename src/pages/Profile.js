86845311ee1e9c791c5085745a7a291780b83ece
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2021-11-13 01:44:05 -0300

    Implementa página do perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import HeaderWithoutSearchBar from '../components/HeaderWithoutSearchBar';
 
 export default function Profile() {
+  useEffect(() => {
+    function setOnStorage() {
+      localStorage.setItem('user', '{ "email": "email@mail.com" }');
+    }
+    return setOnStorage();
+  }, []);
+
   return (
     <>
       <HeaderWithoutSearchBar />
+      <p data-testid="profile-email">{JSON.parse(localStorage.getItem('user')).email }</p>
+      <Link to="/receitas-feitas">
+        <button data-testid="profile-done-btn" type="button">Receitas Feitas</button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ () => localStorage.clear() }
+        >
+          Sair
+        </button>
+      </Link>
+
       <Footer />
     </>
   );
