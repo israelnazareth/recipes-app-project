336ec1586d6bf88918d195d168682b4e88c6146f
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2021-11-21 14:49:29 -0300

    ExploreMeals && ExploreCoktail

--- src/pages/ExploreCocktails.js
+++ src/pages/ExploreCocktails.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import HeaderWithoutSearchBar from '../components/HeaderWithoutSearchBar';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
+import HeaderWithoutSearchBar from '../components/HeaderWithoutSearchBar';
 
 export default function ExploreCocktails() {
+  const history = useHistory();
+  const [id, setId] = useState();
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+      const results = await response.json();
+
+      setId(results.drinks[0].idDrink);
+    };
+    fetchRecipe();
+  }, []);
   return (
     <>
       <HeaderWithoutSearchBar />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push(`/bebidas/${id}`) }
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </>
   );
