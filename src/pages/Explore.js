ee1e799e8e6e4db33b9edefb054016258e9e65e7
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2021-11-24 08:56:17 -0300

    Telas de explorar

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import HeaderWithoutSearchBar from '../components/HeaderWithoutSearchBar';
 
@@ -6,6 +7,16 @@ export default function Explore() {
   return (
     <>
       <HeaderWithoutSearchBar />
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas" data-testid="explore-drinks">
+        <button type="button">
+          Explorar Bebidas
+        </button>
+      </Link>
       <Footer />
     </>
   );
